9a31b4cbc72cc3a125b15f4ac12489be3a61c137
Katerina <ekaterinaignatovich@gmail.com>
2019-01-20 16:39:50 -0500

    Created class "Contact"

---
+++ contact.py
@@ -0,0 +1,16 @@
+class Contact:
+
+    def __init__(self, name, midname, last_name, nick, title, comp_name, address, home_number, email,
+                           date, birth_month, birth_year):
+        self.name = name
+        self.midname = midname
+        self.last_name = last_name
+        self.nick = nick
+        self.title = title
+        self.comp_name = comp_name
+        self.address = address
+        self.home_number = home_number
+        self.email = email
+        self.date = date
+        self.birth_month = birth_month
+        self.birth_year = birth_year
