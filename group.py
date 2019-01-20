b4b41e511982f80a963bbc05dbb63a00108140c3
Katerina <ekaterinaignatovich@gmail.com>
2019-01-20 15:17:08 -0500

    Added "group" constructor

--- group.py
+++ group.py
@@ -1,6 +1,6 @@
-class group:
+class Group:
 
     def __init__(self, name, header, footer):
-        self.name = group_
-        self.header = group_header
-        self.footer = group_footer
+        self.name = name
+        self.header = header
+        self.footer = footer
