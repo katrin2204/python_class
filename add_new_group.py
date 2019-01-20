e1e9d4d0ce69813421a9c003a47eb43c188b9d12
Katerina <ekaterinaignatovich@gmail.com>
2019-01-20 15:04:23 -0500

    Set parameters for classes fill_group_info and login

--- add_new_group.py
+++ add_new_group.py
@@ -14,25 +14,55 @@ class AddNewGroup(unittest.TestCase):
     
     def test_add_new_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
-        wd.find_element_by_link_text("groups").click()
+        # open home page
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_group_page(wd)
+        # init group creation
         wd.find_element_by_name("new").click()
+        self.fill_group_info(wd, "name", "dfzdg", "bgdhg")
+        self.submit_group_creation(wd)
+        self.return_to_group_page(wd)
+        self.logout(wd)
+
+    def fill_group_info(self, wd, name, header, footer):
+        # fill group info
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("group1")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("header")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("footer")
+        wd.find_element_by_name("group_footer").send_keys(footer)
+
+    def submit_group_creation(self, wd):
+        # submit group creation
         wd.find_element_by_name("submit").click()
+
+    def return_to_group_page(self, wd):
+        # group page
         wd.find_element_by_link_text("group page").click()
+
+    def logout(self, wd):
+        # logout
         wd.find_element_by_link_text("Logout").click()
-    
+
+    def open_group_page(self, wd):
+        # open groups page
+        wd.find_element_by_link_text("groups").click()
+
+    def login(self, wd, username, password):
+        # login
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
---
+++ group.py
@@ -0,0 +1,6 @@
+class group:
+
+    def __init__(self, name, header, footer):
+        self.name = group_
+        self.header = group_header
+        self.footer = group_footer
