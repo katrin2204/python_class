ed361291cd513c1fae391a464048e6887da94f2d
Katerina <ekaterinaignatovich@gmail.com>
2019-03-20 06:18:17 -0400

    1. Added preconditions for deletion group test

--- fixture/group.py
+++ fixture/group.py
@@ -67,3 +67,8 @@ class GroupHelper:
         wd = self.app.wd
         # open groups page
         wd.find_element_by_link_text("groups").click()
+
+    def count(self):
+        wd = self.app.wd
+        self.open_group_page()
+        return len(wd.find_elements_by_name("selected[]"))
